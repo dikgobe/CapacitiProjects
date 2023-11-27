d1dfc29f69b66724d7a3c724b9de84782e574533
dikgobe Molepo <djmolepo@gmail.com>
2023-11-27 14:52:56 +0200

    program to add two numbers

---
+++ usingVim.py
@@ -0,0 +1,4 @@
+print("Please enter your numbers")
+num1=float(input("Please enter your first number"))
+num2=float(input("Please enter the second number"))
+print("the sume of the 2 numbers is",num1 +num2)
