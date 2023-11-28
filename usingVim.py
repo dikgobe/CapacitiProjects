442f5bb2f1a3446fd037ac599b3276009e067f1f
dikgobe Molepo <djmolepo@gmail.com>
2023-11-28 15:42:42 +0200

    remove files

--- usingVim.py
+++
@@ -1,4 +0,0 @@
-print("Please enter your numbers")
-num1=float(input("Please enter your first number"))
-num2=float(input("Please enter the second number"))
-print("the sume of the 2 numbers is",num1 +num2)
